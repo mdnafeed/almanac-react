14d8a90e35c632a1766bd75b8300ffa8f2c4590e
Md Faizy <mdgmfaizy@gmail.com>
2024-03-31 14:10:41 +0530

    update

--- src/pages/contact/validationContactSchema.js
+++ src/pages/contact/validationContactSchema.js
@@ -1,11 +1,18 @@
 import * as yup from 'yup';
 const validationContactSchema = yup.object().shape({
   name: yup.string()
-    .matches(/^[A-Za-z\s]+$/, 'name must only contain alphabetical characters and spaces')
-    .required('Name is required'),
-  email: yup.string().email('Invalid email address').required('Email is required'),
-  mobile: yup.string().matches(/^[0-9]{10}$/, 'Invalid mobile number. It must be 10 digits and only contain numbers').required('Enter mobile number'),
-  message: yup.string().required('Message is required')
+  .matches(/^[A-Za-z\s]+$/, 'name must only contain alphabetical characters and spaces')
+  .required('Name is required'),
+  email: yup.string()
+  .email('Invalid email address')
+  .required('Email is required'),
+  mobile: yup.string()
+  .matches(/^[0-9]{10}$/, 'Invalid mobile number. It must be 10 digits and only contain numbers')
+  .required('Enter mobile number'),
+  message: yup.string()
+  .min(3,"Message must be at least 3 characters long")
+  .max(255,"'Message cannot exceed 255 characters")
+  .required('Message is required')
 });
 
 export default validationContactSchema;
